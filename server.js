c5ea71246327752a4ccf582f90f733c96e9b8c9f
David <davidmh.mail@gmail.com>
2019-01-06 04:00:52 -0600

    Adjust deployment configurations

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/pomodime'));
+app.use(express.static(__dirname + '/dist/pomodime2'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname,'/dist/pomodime/index.html'));
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname + '/dist/pomodime2/index.html'));
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
