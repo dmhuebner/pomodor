6e3c886869f8209553dead5a6cd97d4e9f067eaf
David <davidmh.mail@gmail.com>
2019-03-19 22:48:36 -0500

    Implement FormsModule and switch out buttons in settings with FormsModule buttons

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './containers/settings-container/settings.component';
 import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule } from '@angular/material';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { SettingsFormComponent } from './components/settings-form/settings-form.component';
+import { PmFormsModule } from '@shared';
 
 @NgModule({
   declarations: [SettingsComponent, SettingsFormComponent],
   imports: [
     ReactiveFormsModule,
+    FormsModule,
     MatFormFieldModule,
     MatButtonModule,
     MatInputModule,
     MatCheckboxModule,
     CommonModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    PmFormsModule
   ]
 })
 export class SettingsModule { }
